65fa28405e6d5a60b11eddb2b8c07ccabd7a3430
selbetar <sadeqhsb@tuta.io>
2021-02-23 10:07:49 -0800

    support commandline arg for port

--- src/server/Main.hs
+++ src/server/Main.hs
@@ -9,13 +9,25 @@ import Network.Socket
 import qualified Network.Socket.ByteString as S
 import SockNetwork
 import System.IO
+import System.Environment
+import Data.Maybe
+import Text.Read
+
+defaultPortNum :: String
+defaultPortNum = "5000"
 
 main :: IO ()
 main = do
-  sock <- createSock "5000"
-  pr <- socketPort sock
-  putStrLn ("--> FTPServer has started on port " ++ show pr)
-  mainLoop sock
+  args <- getArgs
+  let port = parseArgs args
+  if port == ""
+    then do
+      help
+    else do
+      sock <- createSock port
+      pr <- socketPort sock
+      putStrLn ("--> FTPServer has started on port " ++ show pr)
+      mainLoop sock
 
 mainLoop :: Socket -> IO b
 mainLoop sock = do
@@ -52,3 +64,28 @@ interactFTP sock userState =
   where
     tokens :: String -> [String]
     tokens str = splitsep (`elem` " \t\n\f\r\v") str
+
+-- returns the port number if provided
+-- if no ports specified, returns default port
+-- if an non-numerical value is given, returns an empty string
+parseArgs :: [String] -> String
+parseArgs args
+  | null args = defaultPortNum
+  | otherwise = 
+    if isJust $ maybePort portNum
+      then
+        portNum
+      else
+        ""
+  where
+    portNum = head args
+    maybePort port = readMaybe portNum :: Maybe Int
+
+help :: IO ()
+help = 
+  do
+    progName <- getProgName
+    putStrLn $ "Usage:  " ++ progName ++ " <port>"
+    putStrLn "Specifiy the port number the server should listen to for incoming connections."
+    putStrLn "Or leave it empty for the server to listen on the default port."
+    return ()
